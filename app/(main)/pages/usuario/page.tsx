e7a2ed81276c725ca38eae4a8eae763308d749ce
Antonioafj <antonioafj.edu@gmail.com>
2024-08-04 13:31:19 -0400

    opcao de deletar varios feita

--- app/(main)/pages/usuario/page.tsx
+++ app/(main)/pages/usuario/page.tsx
@@ -5,22 +5,16 @@ import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { Dialog } from 'primereact/dialog';
 import { FileUpload } from 'primereact/fileupload';
-import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber';
 import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
-import { Rating } from 'primereact/rating';
 import { Toast } from 'primereact/toast';
 import { Toolbar } from 'primereact/toolbar';
 import { classNames } from 'primereact/utils';
-import React, { useEffect, useRef, useState } from 'react';
-import { ProductService } from '../../../../demo/service/ProductService';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Projeto } from '@/types';
 import { UsuarioService } from '@/service/UsuarioService';
 import { error } from 'console';
 
-/* @todo Used 'as any' for types here. Will fix in next version due to onSelectionChange event type issue. */
-const Crud = () => {
+const Usuario = () => {
     let usuarioVazio: Projeto.Usuario = {
         id: 0,
         nome: '',
@@ -34,12 +28,12 @@ const Crud = () => {
     const [deleteUsuarioDialog, setDeleteUsuarioDialog] = useState(false);
     const [deleteUsuariosDialog, setDeleteUsuariosDialog] = useState(false);
     const [usuario, setUsuario] = useState<Projeto.Usuario>(usuarioVazio);
-    const [selectedUsuarios, setSelectedUsuarios] = useState(null);
+    const [selectedUsuarios, setSelectedUsuarios] = useState<Projeto.Usuario[]>([]);
     const [submitted, setSubmitted] = useState(false);
     const [globalFilter, setGlobalFilter] = useState('');
     const toast = useRef<Toast>(null);
     const dt = useRef<DataTable<any>>(null);
-    const usuarioService = new UsuarioService();
+    const usuarioService = useMemo(() => new UsuarioService(), []);
 
 
     useEffect(() => {
@@ -51,7 +45,7 @@ const Crud = () => {
             }).catch((error) => {
                 console.log(error);
             })
-    }, [usuarios]);
+    }, [usuarioService, usuarios]);
 
 
     const openNew = () => {
@@ -82,14 +76,14 @@ const Crud = () => {
                     setUsuarioDialog(false);
                     setUsuario(usuarioVazio);
                     setUsuarios([]);
-                    toast.current.show({
+                    toast.current?.show({
                         severity: 'info',
                         summary: 'Sucesso!',
                         detail: 'Usuário cadastrado com sucesso!'
                     });
                 }).catch((error) => {
                     console.log(error.data.message);
-                    toast.current.show({
+                    toast.current?.show({
                         severity:'error',
                         summary: 'error',
                         detail: 'Erro ao salvar!' + error.data.message
@@ -101,14 +95,14 @@ const Crud = () => {
                     setUsuarioDialog(false);
                     setUsuario(usuarioVazio);
                     setUsuarios([]);
-                    toast.current.show({
+                    toast.current?.show({
                         severity: 'info',
                         summary: 'Sucesso!',
                         detail: 'Usuário alterado com sucesso!'
                     });
                 }).catch((error) =>{
                     console.log(error.data.message);
-                    toast.current.show({
+                    toast.current?.show({
                         severity: 'error',
                         summary: 'Erro!',
                         detail: 'Erro ao alterar!' + error.data.message
@@ -129,45 +123,27 @@ const Crud = () => {
     };
 
     const deleteUsuario = () => {
+        if(usuario.id){
         usuarioService.excluir(usuario.id).then((response) => {
                     setUsuario(usuarioVazio);
                     setDeleteUsuarioDialog(false);
                     setUsuarios([]);
-                    toast.current.show({
+                    toast.current?.show({
                     severity: 'success',
                     summary: 'Sucesso',
                     detail: 'Usuario Deletado com Sucesso!',
                     life: 3000});
             }).catch((error) => {
-                toast.current.show({
+                toast.current?.show({
                     severity: 'error',
                     summary: 'Erro!',
                     detail: 'Erro ao deletar o usuário',
-                    life: 3000});   
-        });
-
+                    life: 3000
+                
+                });   
+           });
+        }   
     };
-
-    // const findIndexById = (id: string) => {
-    //     let index = -1;
-    //     for (let i = 0; i < (products as any)?.length; i++) {
-    //         if ((products as any)[i].id === id) {
-    //             index = i;
-    //             break;
-    //         }
-    //     }
-
-    //     return index;
-    // };
-
-    // const createId = () => {
-    //     let id = '';
-    //     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    //     for (let i = 0; i < 5; i++) {
-    //         id += chars.charAt(Math.floor(Math.random() * chars.length));
-    //     }
-    //     return id;
-    // };
 
     const exportCSV = () => {
         dt.current?.exportCSV();
@@ -178,24 +154,31 @@ const Crud = () => {
     };
 
     const deleteSelectedUsuarios = () => {
-        // let _products = (products as any)?.filter((val: any) => !(selectedProducts as any)?.includes(val));
-        // setProducts(_products);
-        // setDeleteProductsDialog(false);
-        // setSelectedProducts(null);
-        // toast.current?.show({
-        //     severity: 'success',
-        //     summary: 'Successful',
-        //     detail: 'Products Deleted',
-        //     life: 3000
-        // });
-    };
 
-    // const onCategoryChange = (e: RadioButtonChangeEvent) => {
-    //     let _product = { ...product };
-    //     _product['category'] = e.value;
-    //     setProduct(_product);
-    // };
-
+        Promise.all(selectedUsuarios.map(async (_usuario) => {
+           if(_usuario.id) {
+              await usuarioService.excluir(_usuario.id);
+            }    
+        })).then((reponse) => {
+            setUsuarios([]);
+            setSelectedUsuarios([]);
+            setDeleteUsuarioDialog(false);
+            toast.current?.show({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Usarios Deletatos Com Sucesso',
+                life: 3000
+            });
+        }).catch((error) => {
+            toast.current?.show({
+                severity: 'error',
+                summary: 'Erro',
+                detail: 'Erro ao deletar usuários',
+                life: 3000
+            })
+    });
+};
+ 
     const onInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, name: string) => {
         const val = (e.target && e.target.value) || '';
         let _usuario = { ...usuario };
@@ -204,13 +187,6 @@ const Crud = () => {
          setUsuario(_usuario);
      };
 
-    // const onInputNumberChange = (e: InputNumberValueChangeEvent, name: string) => {
-    //     const val = e.value || 0;
-    //     let _product = { ...product };
-    //     _product[`${name}`] = val;
-
-    //     setProduct(_product);
-    // };
 
     const leftToolbarTemplate = () => {
         return (
@@ -424,4 +400,4 @@ const Crud = () => {
     );
 };
 
-export default Crud;
+export default Usuario;
